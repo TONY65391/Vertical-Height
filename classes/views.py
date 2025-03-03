f09e028d4a60078e1606ca82688616136007b732
TONY65391 <tonyanthony65391@gmail.com>
2025-03-03 12:47:38 -0800

    first coomit inna while

---
+++ classes/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import loader
+from django.contrib import messages
+
+
+# Create your views here.
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(request = request))
